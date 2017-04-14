c7c69e1dbd1a9033ab55f4314e83ef4505a98adc
AniketPant02 <pant.aniket@gmail.com>
2017-04-14 14:07:56 -0500

    Initialized variables

--- OxygenSimulation.py
+++ OxygenSimulation.py
@@ -4,4 +4,17 @@ Created on Thu Apr 13 23:06:42 2017
 
 @author: Aniket Pant
 """
+# Code must be able to return percentage abundance of O, O2, and O3 after recieving informaiton on cell numbers and molecule amount
+# Needs: Molecule Amount Checker
+
+import math
+
+# Assigning molecule amount to variable "moleculeAmount"
+moleculeAmount = int(input("Enter molecule amount here: "))
+
+# Assigning cell amount to variable "cellAmount"
+cellAmount = int(input("Enter cell amount to be populated: "))
+
+# Assigning photon amount to variable "photonAmount"
+photonAmount = int(input("Enter photon amount: "))
 
