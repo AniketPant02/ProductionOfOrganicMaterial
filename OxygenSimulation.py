d538107bd29ef86b85a31396bf927d7e31f2ce14
AniketPant02 <pant.aniket@gmail.com>
2017-04-22 14:23:55 -0500

    Added random number generation
    Added random number generation

--- OxygenSimulation.py
+++ OxygenSimulation.py
@@ -10,6 +10,7 @@ Created on Thu Apr 13 23:06:42 2017
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 # Assigning molecule amount to variable "moleculeAmount"
 moleculeAmount = int(input("Enter molecule amount here: "))
@@ -20,3 +21,37 @@ cellAmount = int(input("Enter cell amount to be populated: "))
 # Assigning photon amount to variable "photonAmount"
 photonAmount = int(input("Enter photon amount: "))
 
+runCount = 1
+MonoOxygenCount = 0
+DiOxygenCount = 0
+TriOxygenCount = 0
+
+while (runCount < 1000):
+    photonHitRate = random.random() # photonHitRate decides which test-case is to be selected for simulation evaluation
+
+    if (photonHitRate < 0.273):
+        print("Hit O")
+        runCount += 1
+        MonoOxygenCount += 1
+    elif (0.273 < photonHitRate < 0.841):
+        print("Hit O2")
+        runCount += 1
+        DiOxygenCount += 1
+    elif (0.841 < photonHitRate < 1.0):
+        print("Hit O3")
+        runCount += 1
+        TriOxygenCount += 1
+
+MonoOxygenPercent = MonoOxygenCount/runCount
+DiOxygenPercent = DiOxygenCount/runCount
+TriOxygenPercent = TriOxygenCount/runCount
+
+print("Following includes the Oxygen level counts per molecule type")
+print(MonoOxygenCount)
+print(DiOxygenCount)
+print(TriOxygenCount)
+
+print("Following includes the Oxygen percent levels repective to molecule type")
+print(MonoOxygenPercent)
+print(DiOxygenPercent)
+print(TriOxygenPercent)
